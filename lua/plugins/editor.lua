f4d447860488ddec24b18b278761c10cc6f05e50
Liu Yang <liuyangzys@outlook.com>
2025-01-13 13:20:01 +0800

    删除无用的cmp配置

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -1,34 +1,5 @@
 return {
   {
-    "hrsh7th/nvim-cmp",
-    event = { "CmdlineEnter", "InsertEnter" },
-    dependencies = {
-      "hrsh7th/cmp-cmdline",
-      config = function()
-        local cmp = require("cmp")
-        ---@diagnostic disable-next-line: missing-fields
-        -- `:` cmdline setup.
-        cmp.setup.cmdline(":", {
-          mapping = cmp.mapping.preset.cmdline(),
-          completion = { completeopt = "noselect" },
-          sources = cmp.config.sources(
-            { { name = "path" } },
-            { { name = "cmdline", option = { ignore_cmds = { "Man", "!" } } } }
-          ),
-        })
-        -- `/` cmdline setup.
-        cmp.setup.cmdline("/", {
-          mapping = cmp.mapping.preset.cmdline(),
-          completion = { completeopt = "noselect" },
-          sources = {
-            { name = "buffer" },
-          },
-        })
-      end,
-    },
-  },
-
-  {
     "tpope/vim-fugitive",
     cmd = {
       "G",
