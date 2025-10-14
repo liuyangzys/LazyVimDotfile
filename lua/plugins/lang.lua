b6f6e25391af5003805a4e95e374b9b559381095
Liu Yang <liuyangzys@outlook.com>
2025-10-14 19:11:26 +0800

    使用本地clangd lsp

--- lua/plugins/lang.lua
+++ lua/plugins/lang.lua
@@ -3,4 +3,10 @@ return {
     "iamcco/markdown-preview.nvim",
     build = "cd app && yarn install",
   },
+  {
+    "neovim/nvim-lspconfig",
+    opts = function(_, opts)
+      opts.servers.clangd.mason = false
+    end,
+  },
 }
